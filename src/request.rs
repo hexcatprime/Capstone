6678d1b5641c71e732353e9492f196cce8de7a3f
batistinix <batistinix@wit.edu>
2024-07-23 22:20:22 -0400

    Implemented request function.

--- src/request.rs
+++ src/request.rs
@@ -1,17 +1,29 @@
-use reqwest;
+use reqwest::{self, Response};
 
-#[tokio::main(flavor = "current_thread")]
-async fn query() {
-    //let client = reqwest::Client::new();
+#[tokio::main]
+async fn search(title: String, year: Option<i32>) -> String{
+    let api_key = "&apikey=f95d2eac";
+    let mut request_body = format!("https://www.omdbapi.com/?t={}]", title);
 
-    let response = reqwest::get("https://www.omdbapi.com/?t=Dune&y=2021&apikey=XXXXXXXX")
+    if year.is_some() {
+        let request_year = format!("&y={}", year.unwrap().to_string());
+        request_body += &request_year;
+    }
+
+    request_body += api_key;
+
+    let response = reqwest::get(request_body)
         .await
         .unwrap()
         .text()
         .await;
-    println!("{:?}", response);
-}
+    return response.unwrap();
+} 
 
+//test function
 fn main() {
-    query();
+    println!("Raw HTTP response:");
+    println!("{}\n", search("superbad".to_string(), None));
+
+
 }
